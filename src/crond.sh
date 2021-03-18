d6c7333256a0c77ea910444dd59417394e4076b1
zhangwei <354190012@qq.com>
2021-03-18 18:28:35 +0800

    修改bug

--- src/crond.sh
+++ src/crond.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 echo 通过模拟打开网站获取数据，定时，每2小时执行一次
 dir=`dirname $0`
+dir=`dirname $dir`
 [ ! -f $dir/package.json ] && {
-    echo 找不到文件，请确认代码是否完整
+    echo 找不到文件$dir/package.json，请确认代码是否完整
     exit
 }
 cd $dir
