548796a3286c75eb80aa327b99fce5f8359f7804
zhangwei <354190012@qq.com>
2021-03-18 18:02:23 +0800

    'ds'

---
+++ src/crond.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo 通过模拟打开网站获取数据，定时，每2小时执行一次
+cd ..
+echo 获取今天数据
+npm run openUrl http://www.bolool.com:8080/bolool.html
+echo 获取昨天数据
+npm run openUrl http://www.bolool.com:8080/bolool.html?date=${date -d 'yesterday' +"%F"}
+echo 获取前天数据
+npm run openUrl http://www.bolool.com:8080/bolool.html?date=${date -d '2 days ago' +"%F"}
+
+echo 执行完成
